81ff72638fe9b8f3e9f4588512ee489137ef3cf8
boyka <yangpei@alianying.com>
2018-10-19 11:08:20 +0800

    Initial Commit

---
+++ iYuBaHomePageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iYuBaHomePageDemo
+//
+//  Created by Boyka on 2018/10/19.
+//  Copyright © 2018年 WQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
